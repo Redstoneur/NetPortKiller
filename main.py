9c7ee350e11fec10e9f12ad54e52c53d6a357069
Redstoneur <alfa.aog36@gmail.com>
2025-06-08 17:40:01 +0200

    Add dynamic icon path handling for PyInstaller executable

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 # Main entry point for the NetPortKiller application.
 # This script initializes and launches the application's graphical interface.
 
-from netportkiller.gui import NetPortKillerApp
 import os
+import sys
+
+from netportkiller.gui import NetPortKillerApp
+
+if hasattr(sys, "_MEIPASS"):
+    # Exécution depuis l'exécutable PyInstaller
+    icon_path = os.path.join(sys._MEIPASS, "assets", "icon.ico")
+else:
+    # Exécution depuis le code source
+    icon_path = os.path.abspath("./assets/icon.ico")
 
 
 def main() -> None:
@@ -12,7 +21,6 @@ def main() -> None:
     This function creates an instance of the graphical application and starts the main loop
     to display the user interface and handle user interactions.
     """
-    icon_path = os.path.abspath("./assets/icon.ico")
     # Create an instance of the NetPortKiller graphical application with icons
     app = NetPortKillerApp(icon_path=icon_path)
     # Start the application's main loop.
